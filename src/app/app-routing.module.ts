02c7322c0b8099034c2fbe2a5709aa0f661e5202
Lucas Piva <lucas.p.dias@outlook.com>
2021-08-25 12:51:56 -0300

    Gerenciando rotas: entrar e cadastre-se

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
+import { EntrarComponent } from './entrar/entrar.component';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
+
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'entrar',pathMatch: 'full'}, //qdo o caminho for vazio, ele redireciona para o entrar
+  {path: 'entrar', component: EntrarComponent},
+  {path: 'cadastrar', component: CadastrarComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
